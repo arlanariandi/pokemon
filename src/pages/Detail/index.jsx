ffba71ba8e54fe39d49f55bcead3e17abea536c3
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2024-08-05 15:41:59 +0700

    [UPDATE]: Update loading animation

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -54,59 +54,17 @@ const Detail = () => {
                 return 'bg-types-normal';
         }
     }
-    const getPokemonElementEvolution = (type) => {
-        switch (type) {
-            case 'bug':
-                return 'text-types-bug';
-            case'grass':
-                return 'text-types-grass';
-            case 'dragon':
-                return 'text-types-dragon';
-            case 'steal':
-                return 'text-types-steal';
-            case 'dark':
-                return 'text-types-dark';
-            case 'flying':
-                return 'text-types-flying';
-            case 'normal':
-                return 'text-types-normal';
-            case 'ghost':
-                return 'text-types-ghost';
-            case 'rock':
-                return 'text-types-rock';
-            case 'ground':
-                return 'text-types-ground';
-            case 'fighting':
-                return 'text-types-fighting';
-            case 'fire':
-                return 'text-types-fire';
-            case 'electric':
-                return 'text-types-electric';
-            case 'poison':
-                return 'text-types-poison';
-            case 'psychic':
-                return 'text-types-psychic';
-            case 'fairy':
-                return 'text-types-fairy';
-            case 'water':
-                return 'text-types-water';
-            case 'ice':
-                return 'text-types-ice';
-            default:
-                return 'text-types-normal';
-        }
-    }
 
     if (isLoading) return <Loading/>
 
     return (
         <div className="bg-types-dark/10">
-            <Navbar/>
+            <Navbar title="Detail Pokemon"/>
             <div className="px-4 my-6">
                 <PokemonDetail pokemonDetail={pokemonDetail} pokemonElement={getPokemonElement}/>
                 <PokemonBreeding detail={pokemonDetail}/>
                 <PokemonAbilities abilitiesDetail={pokemonDetail}/>
-                <PokemonEvolution pokemonEvolution={pokemonDetail} pokemonElementEvolution={getPokemonElementEvolution}/>
+                <PokemonEvolution pokemonEvolution={pokemonDetail}/>
             </div>
             <Footer/>
         </div>
